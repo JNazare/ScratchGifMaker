8211d6e6dfca71ded62459bc138ad4b398dc6498
jnazare <jules.nazare@gmail.com>
2015-05-20 19:25:48 -0400

    working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,10 @@
-angular.module('app.controllers', ['ngCordova'])
- 
-.controller('VideoCtrl', function($scope, $cordovaCapture) {
+var app = angular.module('app.controllers', ['ngCordova'])
+
+var emailTemplate = '<html><head></head><body style="font-family: sans-serif;"><p>Attached is your GIF. There are a few more steps you need to import your GIF into Scratch.</p><ol><li> Download your GIF </li><li> Upload your GIF as a sprite in a Scratch Project </li></ol>Thanks!</body></html>';
+
+app.controller('VideoCtrl', function($scope, $cordovaCapture, $ionicPopup, $http) {
     console.log("in ctrl");
-    $scope.clip = '';
+    $scope.clip = 'img/blankgif_instructions.png';
 
     // capture callback
 	var captureSuccess = function(mediaFiles) {
@@ -17,8 +19,14 @@ angular.module('app.controllers', ['ngCordova'])
         ft.upload(videoURI, encodeURI("http://18.85.25.156:5000/"), 
 	        // Succes
 	        function(succes){
-	            alert(succes.response);
-	            console.log(succes.response);
+	        	// console.log("data:image/gif;base64,"+succes.response);
+	        	console.log('here');
+	        	setTimeout(function () {
+			        $scope.$apply(function () {
+			        	$scope.base64_gif = succes.response;
+			            $scope.clip="data:image/gif;base64,"+$scope.base64_gif;
+			        });
+			    }, 2000);
 	        }, 
 	        function(error){
 	            alert(error.target);
@@ -38,5 +46,76 @@ angular.module('app.controllers', ['ngCordova'])
 		var options = { "duration": 10 };
 		navigator.device.capture.captureVideo(captureSuccess, captureError, options);
 	};
+
+	$scope.deleteGif = function() {
+		$scope.clip = 'img/blankgif_instructions.png';
+	}
+
+	$scope.sendGif = function(){
+		$scope.user = {};
+		if($scope.clip != 'img/blankgif_instructions.png'){
+			var emailPopup = $ionicPopup.show({
+		    template: '<input type="email" ng-model="user.email">',
+		    title: 'Enter Your Email',
+		    subTitle: 'We will send you your video to import into Scratch',
+		    scope: $scope,
+		    buttons: [
+		      { text: 'Cancel' },
+		      {
+		        text: '<b>Send</b>',
+		        type: 'button-positive',
+		        onTap: function(e) {
+		          if (!$scope.user.email) {
+		            //don't allow the user to close unless he enters wifi password
+		            e.preventDefault();
+		          } else {
+		            return $scope.user.email;
+		          }
+		        }
+		      }
+		    ]
+		  });
+		  emailPopup.then(function(res) {
+		  	email_options = {
+			  		"key": "my key",
+	    			"message": {
+	      				"from_email": "juliana@media.mit.edu",
+		      			"to": [
+		          			{
+		            			"email": res
+		          			}],
+		      			// "autotext": "true",
+		      			"subject": "Your video to upload to Scratch",
+		      			"html": emailTemplate,
+		      			"track_opens": false,
+		      			"track_clicks": false,
+		      			"images": [
+			            {
+			                "type": "image/gif",
+			                "name": "scratchgif",
+			                "content": $scope.base64_gif
+			            }
+			        ]
+			  		}
+		  		}
+		    $http.post('https://mandrillapp.com/api/1.0/messages/send.json', email_options).success(function(data, status, headers, config) {
+			    console.log(data);
+			    // this callback will be called asynchronously
+			    // when the response is available
+			  }).
+			  error(function(data, status, headers, config) {
+			  	console.log("error");
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+			  });
+		  });
+		}
+		else{
+			var alertPopup = $ionicPopup.alert({
+		     title: 'Oops!',
+		     template: 'Please create a video first.'
+		   });
+		}
+	}
 
 });
